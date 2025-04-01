6f08d8ddd54691ed0aeab65be8c00fb6981378a2
limsejo <jselim234@gmail.com>
2025-04-02 00:20:28 +0900

    [add] 점프, 착지, 달리기 애니메이션 작업

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D PlayerRigidBody;
+    public Animator PlayerAnimator;
 
     private bool isGrounded = true; // 땅에 있는지 체크하는 변수
 
@@ -25,13 +26,17 @@ public class Player : MonoBehaviour
             // 점프
             PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            PlayerAnimator.SetInteger("state", 1);
         }
     }
 
-    // 두 개의 Collider가 충돌하면 자동으로 호출되는 메서드
+    // 두 개의 Collider가 충돌하면 자동으로 호출되는 메서드 (게임 시작하자마자 실행)
     void OnCollisionEnter2D(Collision2D collision) {
         
         if (collision.gameObject.name == "Platform") {
+            if (!isGrounded) {
+                PlayerAnimator.SetInteger("state", 2);
+            }
             isGrounded = true;
         }
     }
